4abd50caa9725bdcc543ba5d82307df9c146f26e
norbertkis <kis.norbert.g.95@gmail.com>
2021-03-12 16:34:33 +0100

    Login connect mySQL

---
+++ src/main/java/hu/kisno/DatabaseConnection.java
@@ -0,0 +1,28 @@
+package hu.kisno;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+
+public class DatabaseConnection {
+    public Connection databaseLink;
+
+    public Connection getConnection(){
+        String databaseName = "todo";
+        String databaseUser = "root";
+        String databasePassword = "Kn_19950801";
+        String url = "jdbc:mysql://localhost:3306/" + databaseName;
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            databaseLink = DriverManager.getConnection(url, databaseUser, databasePassword);
+
+        }catch (Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+
+        return databaseLink;
+
+    }
+
+}
--- src/main/java/hu/kisno/LoginController.java
+++ src/main/java/hu/kisno/LoginController.java
@@ -1,6 +1,8 @@
 package hu.kisno;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -8,22 +10,22 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 
-public class LoginController {
+public class LoginController implements Initializable {
 
     public void close(MouseEvent event) {
         System.exit(0);
     }
 
     @FXML
-    private ResourceBundle resources;
+    private Button loginButton;
 
     @FXML
-    private URL location;
-
-    @FXML
-    private Label labelClose;
+    private Label loginMassageLabel;
 
     @FXML
     private TextField loginUsername;
@@ -31,20 +33,44 @@ public class LoginController {
     @FXML
     private PasswordField loginPassword;
 
-    @FXML
-    private Button loginButton;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+    }
 
-    @FXML
-    private Button loginSignUpButton;
+    public void loginButtonOnAction(ActionEvent event){
+        if(loginUsername.getText().isBlank() == false && loginPassword.getText().isBlank() == false) {
+            validateLogin();
+        }else{
+            loginMassageLabel.setText("Please enter username and password!");
+        }
+    }
 
-    @FXML
-    void initialize() {
-        loginButton.setOnAction(event -> {
-            System.out.println("Clicked Login Button!");
-        });
-        loginSignUpButton.setOnAction(event -> {
-            System.out.println("Clicked Sign Up Button!");
-        });
+    public void validateLogin(){
+        DatabaseConnection connectNow = new DatabaseConnection();
+        Connection connectDB = connectNow.getConnection();
+
+        String verifyLogin = "SELECT count(1) FROM users WHERE username = '" + loginUsername.getText()
+                + "' AND password = '" + loginPassword.getText() + "'";
+
+        try{
+
+            Statement statement = connectDB.createStatement();
+            ResultSet queryResult = statement.executeQuery(verifyLogin);
+
+            while (queryResult.next()){
+                if(queryResult.getInt(1) ==  1){
+                    loginMassageLabel.setText("Congratulations!");
+                }else{
+                    loginMassageLabel.setText("Invalid login. Please try again. ");
+                }
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
 
     }
+
+
 }

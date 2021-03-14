ae9e6867073e55cc44c528a04f3b15c9772b3038
norbertkis <kis.norbert.g.95@gmail.com>
2021-03-14 10:50:09 +0100

    successful signup -> login screen

--- src/main/java/hu/kisno/SignUpController.java
+++ src/main/java/hu/kisno/SignUpController.java
@@ -2,13 +2,19 @@ package hu.kisno;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -56,6 +62,21 @@ public class SignUpController implements Initializable {
             statement.executeUpdate(insertRegister);
 
             registrationMassageLabel.setText("User has been registreted successfully!");
+
+            wait(50);
+
+            try {
+
+                signUpButton.getScene().getWindow().hide();
+                Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+                Stage registerStage = new Stage();
+                registerStage.initStyle(StageStyle.UNDECORATED);
+                registerStage.setScene(new Scene(root, 700, 400));
+                registerStage.showAndWait();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
         }catch (Exception e){
             e.printStackTrace();
